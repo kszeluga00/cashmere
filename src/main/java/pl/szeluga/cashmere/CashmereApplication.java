c375c02e8f7a4ef691c031322e14d5e475bb7eec
kszeluga <kszeluga@gmail.com>
2023-11-23 22:23:06 +0100

    innit

---
+++ src/main/java/pl/szeluga/cashmere/CashmereApplication.java
@@ -0,0 +1,13 @@
+package pl.szeluga.cashmere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashmereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashmereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/szeluga/cashmere/CashmereApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szeluga.cashmere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashmereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
